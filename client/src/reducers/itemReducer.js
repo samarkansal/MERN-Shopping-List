ec1c6b553b3c7cefc69a0634e92dd2e36b3c316b
Samar Kansal <sameerkansal27@gmail.com>
2020-05-06 16:24:38 +0530

    make it better

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,34 @@
+import * as types from "../actions/types";
+
+const initialState = {
+  items: [],
+  loading: false,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case types.GET_ITEMS:
+      return {
+        ...state,
+        items: action.payload,
+        loading: false,
+      };
+    case types.DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter((item) => item._id != action.payload),
+      };
+    case types.ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items],
+      };
+    case types.ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+}
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://padosi1:padosi1@cluster0-jzcr0.mongodb.net/shoplist?retryWrites=true&w=majority",
+};
